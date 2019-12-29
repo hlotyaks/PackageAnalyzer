06657480afa548224836db7343fca629d775f5a5
hlotyaks <hlotyaks@gmail.com>
2019-12-29 14:43:15 -0500

    setting up skeleton code

---
+++ GraphBuilder/tests/Graph.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PackageAnalyzer.Graph.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
