f7ae1f62f9ec6d5ed95deea41e75d7058d2a5aca
hlotyaks <hlotyaks@gmail.com>
2020-01-02 13:02:56 -0500

    add package hasher - skeleton

---
+++ PackageHasher/src/PackageHasher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace PackageAnalyzer
+{
+    public class PackageHasher
+    {
+        public Dictionary<string, string> HashFolders(List<string> folders)
+        {
+            Dictionary<string, string> hasedFolders = new Dictionary<string, string>();
+
+
+            return hasedFolders;
+        }
+        async Task<string> HashFolder(DirectoryInfo folder, bool excludeRoot, string searchPattern = "*", SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            string rootPath = String.Empty;
+
+            if(excludeRoot)
+            {
+                rootPath = folder.FullName.Substring(0, folder.FullName.Length - folder.FullName.Split('\\').Last().Length );
+            }   
+
+            using(var alg = MD5.Create())
+            {
+
+                var result = await alg.ComputeHashAsync(folder.EnumerateFiles(searchPattern, searchOption), rootPath);
+
+                // Build the final string by converting each byte
+                // into hex and appending it to a StringBuilder
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    sb.Append(result[i].ToString("X2"));
+                }
+
+                // And return it
+                return sb.ToString();
+
+            }
+        }
+    }
+}
---
+++ PackageHasher/tests/PackageHaser.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PackageAnalyzer.PackageHasher.Tests
+{
+
+    [TestClass]
+    public class PackageHasherTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+}
